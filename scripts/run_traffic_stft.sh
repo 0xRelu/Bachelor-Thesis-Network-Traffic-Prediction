6313163ea4346535dc97d24ff725fef24350d18f
Nicolas <nicobra1410@gmx.de>
2023-11-22 11:29:01 +0100

    Update to scripts

---
+++ scripts/run_traffic_stft.sh
@@ -0,0 +1,15 @@
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/baseline/config_dlinear.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/baseline/config_linear.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/baseline/config_rlinear.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/baseline/config_nlinear.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_vanilla.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_informer.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_non_stationary.yaml
+
+python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_patchtst.yaml
