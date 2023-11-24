31f8b275993686e7fed185d808effbed994fb91f
Nicolas <nicobra1410@gmx.de>
2023-11-24 12:41:31 +0100

    Configuration scripts final

--- scripts/run_traffic_stft.sh
+++ scripts/run_traffic_stft.sh
@@ -10,6 +10,6 @@ python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/
 
 python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_informer.yaml
 
-python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_non_stationary.yaml
+# python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_non_stationary.yaml
 
-python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_patchtst.yaml
+# python LtsfExperiment.py -s -o ~/BA/BA_LTSF_w_Transformer/conf/stft/transformer/config_patchtst.yaml
