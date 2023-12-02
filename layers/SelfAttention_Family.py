Bugfix add masking again

layers/SelfAttention_Family.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 import numpy as np
 from math import sqrt
-from utils.masking import TriangularCausalMask, ProbMask
 
+from utils.masking import TriangularCausalMask, ProbMask
 
 
 class FullAttention(nn.Module):