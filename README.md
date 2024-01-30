256cbc94fef52dee7d5e47d82a33926d85164a97
Nicolas Braun <npc@Air-von-Nicolas.fritz.box>
2024-01-30 12:01:02 +0100

    Update readme and data preperation

--- README.md
+++ README.md
@@ -1,15 +1,50 @@
+# Evaluating Long-Term Time-Series Forecasting Methods for Network Traffic Prediction
 
-This is the codebase for the paper:
+Python 3.6
+![PyTorch 1.2](https://img.shields.io/badge/PyTorch%20-%23EE4C2C.svg?style=plastic)
+![cuDNN 7.3.1](https://img.shields.io/badge/cudnn-7.3.1-green.svg?style=plastic)
+
+This is the origin Pytorch implementation of the following paper:
+[Evaluating Long-Term Time Series Forecasting Methods for Network Traffic Prediction](Link)
+
+## Content
+
+:triangular_flag_on_post: Preprocessing of network traffic
+:triangular_flag_on_post: STFT_Framework model
+:triangular_flag_on_post: Slurm configuration to run different models on the dataset
+:triangular_flag_on_post: Includes Nonstationary_Transformers, Informer, PatchTST, NLinear, DLinear, RLinear, Linear
+
+## Data
+
+The dataset was taken from the paper [Network Traffic Characteristics of Data Centers in the Wild](https://pages.cs.wisc.edu/~tbenson/IMC10_Data.html). Only the UNI1 packet traces dataset was used. However the other packet traces datasets (UNI1, PRV1) or any packet traces saved in a pcap-file can be used with the introduced preprocessing methods.
+
+## Preprocessing
+
+1. Download packet trace dataset from [the website](https://pages.cs.wisc.edu/~tbenson/IMC10_Data.html)
+2. Save pcap-files in the data repository (optionally in another subdirectory).
+3. Configure parameter in the data_provider/data_preparer main-method (e.g. path/to/pcapFiles and so on).
+4. Run data_provider/data_preparer main-method - Now the resulting flows (aggregated) should be saved in data directory.
+5. Search in the conf/\* directory for the wanted configuration and update parameter like paths (!!) to the location where the data was saved in the last step (4).
+6. Optionally change hyperparameters
+7. Run
+
+```
+python LtsfExperiment.py -o <path/to/config.yaml>
+```
+
+## Potential Errors
+
+- Wrong Paths in (be careful with / and \) config or data_preparer
+- Wandb has to be configured
+
+## Acknowledgemends:
+
 Parts of the code were taken from the following repositories:
+
 - https://github.com/thuml/Autoformer (Vanilla Transformer, Informer, and general structures)
 - https://github.com/PatchTST/PatchTST (PatchTST)
 - https://github.com/thuml/Nonstationary_Transformers (Non-Stationary Transformer)
 - https://github.com/cure-lab/LTSF-Linear (DLinear and NLinear)
 - https://github.com/plumprc/RTSF (RLinear)
 
----
-
-# Run application
-0. To parse pcap-file to python object used by the DataPreparation: /utils/data_preparation_tools.py run parse_pcap_to_list_n
-1. After that create flow time series: run /data_provider/data_preparer.py with right path
-2. After that, choose the correct configuration from the conf folter and run: python LtsfExperiment.py -o <yourconfig.yaml>
+Thanks for the computing infrastructure provided by the Scientific computing center [SCC](https://www.scc.kit.edu/).
--- data_provider/data_preparer.py
+++ data_provider/data_preparer.py
@@ -1,5 +1,7 @@
 import abc
 import csv
+from os import listdir
+from os.path import isfile, join
 import pickle
 
 import datetime
@@ -9,31 +11,39 @@ from statistics import mean
 import numpy as np
 import torch
 
-from utils.data_preparation_tools import parse_pcap_to_list_n, split_tensor_gpu
+from utils.data_preparation_tools import (
+    create_test_from_full,
+    parse_pcap_to_list_n,
+    split_tensor_gpu,
+)
 
 
 class DataTransformerBase:
     def __init__(self, file_path: str):
         self.file_path = file_path
 
-        if not file_path.endswith('.pkl'):
-            raise AttributeError('Muss ein phl file sein. Wenn es noch ein pcap file ist. Parse mit Methode!')
+        if not file_path.endswith(".pkl"):
+            raise AttributeError(
+                "Muss ein phl file sein. Wenn es noch ein pcap file ist. Parse mit Methode!"
+            )
 
         self.packets = self._load_packet_from_pkl(file_path)
         self.data = self._get_data(data_flows=self.packets)
 
     def _load_packet_from_pkl(self, file_path) -> list:
         print(f"[+] Loading packets from pkl file with location: {self.file_path} ...")
-        with open(file_path, 'rb') as f:
+        with open(file_path, "rb") as f:
             data = pickle.load(f)
 
         return data
 
     def save_python_object(self, py_save_path: str) -> str:
-        with open(py_save_path, 'wb') as f:
+        with open(py_save_path, "wb") as f:
             pickle.dump(self.data, f)
 
-        print(f"[+] Wrote {len(self.data)} successfully in file with path {py_save_path}")
+        print(
+            f"[+] Wrote {len(self.data)} successfully in file with path {py_save_path}"
+        )
         return py_save_path
 
     def save_csv(self, csv_save_path: str):
@@ -41,7 +51,9 @@ class DataTransformerBase:
             writer = csv.writer(file)
             writer.writerows(self.data)
 
-        print(f"[+] Wrote {len(self.data)} successfully in file with path {csv_save_path}")
+        print(
+            f"[+] Wrote {len(self.data)} successfully in file with path {csv_save_path}"
+        )
 
     @abc.abstractmethod
     def _get_data(self, data_flows: list[np.ndarray]):
@@ -49,7 +61,13 @@ class DataTransformerBase:
 
 
 class DatatransformerEvenSimpleGpu(DataTransformerBase):
-    def __init__(self, file_path: str, consecutive_zeros=500, min_length: int = 1000, aggr: int = 1000):
+    def __init__(
+        self,
+        file_path: str,
+        consecutive_zeros=500,
+        min_length: int = 1000,
+        aggr: int = 1000,
+    ):
         self.aggr = aggr
         self.min_length = min_length
         self.consecutive_zeros = consecutive_zeros
@@ -61,7 +79,7 @@ class DatatransformerEvenSimpleGpu(DataTransformerBase):
         keys = list(data_flows.keys())
 
         if filter_tcp:
-            keys = [k for k in keys if k[0].startswith('TCP')]
+            keys = [k for k in keys if k[0].startswith("TCP")]
             print(f"[+] Found {len(keys)} flows with filter tcp.")
 
         # if filter_min_length is not None:
@@ -97,7 +115,9 @@ class DatatransformerEvenSimpleGpu(DataTransformerBase):
         data_flows = list(data_flows.values())
 
         for i in range(len(data_flows)):
-            data_flows[i] = torch.tensor([x[:2] for x in data_flows[i]], device=device, dtype=torch.float64)
+            data_flows[i] = torch.tensor(
+                [x[:2] for x in data_flows[i]], device=device, dtype=torch.float64
+            )
 
         flow_seq = []
 
@@ -106,36 +126,62 @@ class DatatransformerEvenSimpleGpu(DataTransformerBase):
         for flow in data_flows:
             start_time = int(flow[0, 0] * self.aggr)  # assumes packets are ordered
             end_time = int(flow[-1, 0] * self.aggr) + 1
-            flow_series_bytes = torch.zeros(end_time - start_time + 1, device=device, dtype=torch.float64)
+            flow_series_bytes = torch.zeros(
+                end_time - start_time + 1, device=device, dtype=torch.float64
+            )
 
             if flow_series_bytes.shape[0] < self.min_length:
                 counter += 1
                 continue
 
-            flow_series_time = torch.arange(start_time, end_time + 1, device=device, dtype=torch.float64)
+            flow_series_time = torch.arange(
+                start_time, end_time + 1, device=device, dtype=torch.float64
+            )
 
             packet_times = ((flow[:, 0] * self.aggr) - start_time).long()
             flow_series_bytes.index_add_(0, packet_times, flow[:, 1])
-            flow_series_bytes = torch.stack((flow_series_time / self.aggr, flow_series_bytes), dim=1)
-            flow_series_bytes = split_tensor_gpu(flow_series_bytes, consecutive_zeros=self.consecutive_zeros)
-            flow_series_bytes = [f.to('cpu') for f in flow_series_bytes if f.shape[0] > self.min_length]
+            flow_series_bytes = torch.stack(
+                (flow_series_time / self.aggr, flow_series_bytes), dim=1
+            )
+            flow_series_bytes = split_tensor_gpu(
+                flow_series_bytes, consecutive_zeros=self.consecutive_zeros
+            )
+            flow_series_bytes = [
+                f.to("cpu") for f in flow_series_bytes if f.shape[0] > self.min_length
+            ]
             flow_seq.extend(flow_series_bytes)
 
             if counter % 1000 == 0:
                 print(
-                    f"[+] Found {len(flow_seq)} in {counter / len(data_flows)} % | Splitted Flow Length Mean {mean([0] + [len(x) for x in flow_seq])}")
+                    f"[+] Found {len(flow_seq)} in {counter / len(data_flows)} % | Splitted Flow Length Mean {mean([0] + [len(x) for x in flow_seq])}"
+                )
             counter += 1
 
-        print(f"[+] Found sequences: {len(flow_seq)} in {len(data_flows)} flows. "
-              f"Real preds: {len([x for x in flow_seq if x.shape[0] > 2 * self.consecutive_zeros]) / len(flow_seq)} Parsing timestamps....")
+        print(
+            f"[+] Found sequences: {len(flow_seq)} in {len(data_flows)} flows. "
+            f"Real preds: {len([x for x in flow_seq if x.shape[0] > 2 * self.consecutive_zeros]) / len(flow_seq)} Parsing timestamps...."
+        )
 
         def mapping2(time):
-            return [time.month, time.day, time.weekday(), time.hour, time.minute, time.second, time.microsecond // 1000,
-                    time.microsecond % 1000]
+            return [
+                time.month,
+                time.day,
+                time.weekday(),
+                time.hour,
+                time.minute,
+                time.second,
+                time.microsecond // 1000,
+                time.microsecond % 1000,
+            ]
 
         def mapping(x):
             time = datetime.datetime.fromtimestamp(x[0, 0].item())
-            time_in_tensor = np.array([mapping2(time + datetime.timedelta(milliseconds=t_d)) for t_d in range(x.shape[0])])
+            time_in_tensor = np.array(
+                [
+                    mapping2(time + datetime.timedelta(milliseconds=t_d))
+                    for t_d in range(x.shape[0])
+                ]
+            )
             features = x[:, 1:].detach().numpy()
 
             return [time_in_tensor, features]
@@ -151,47 +197,41 @@ class DatatransformerEvenSimpleGpu(DataTransformerBase):
         return flow_seq
 
 
-def _create_split_flow_files():
-    path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1\\univ1_pt'  # _test
-    path = [path + str(i) for i in range(1, 21)]
-    path_test = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1\\univ1_pt1_test'  #
-
-    save_path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1\\univ1_pt_full_2.pkl'
-    save_path_test = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1\\univ1_pt1_test.pkl'
-
-    parse_pcap_to_list_n(path, save_path)
-    # parse_pcap_to_list_n([path_test], save_path_test)cd
-
-
 def _save_even_gpu(load_path: str, save_path: str, aggr_time: list):
     for j in aggr_time:
         save_path_ = save_path + f"_{j}.pkl"
-        data_transformer = DatatransformerEvenSimpleGpu(load_path, consecutive_zeros=500, min_length=800, aggr=j)
+        data_transformer = DatatransformerEvenSimpleGpu(
+            load_path, consecutive_zeros=500, min_length=800, aggr=j
+        )
 
         data_transformer.save_python_object(save_path_)
         print(f"[x] Finished aggr {j} and saved it in {save_path_}")
 
-    print("<<<<<<<<<<<<<<<< Done Even >>>>>>>>>>>>>>>>")
-
 
 if __name__ == "__main__":
-    print("<<<<<<<<<<<<<<<< Start >>>>>>>>>>>>>>>>")
-    path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt_full.pkl'  # univ1_pt_n
-    save_path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt1_even4'
+    print(
+        "<<<<<<<<<<<<<<<< Start - be aware that certain parameters need to be configured >>>>>>>>>>>>>>>>"
+    )
+    all = True  # True if all Flows should be used - False if they should be filtered
 
-    test_path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt_n_test.pkl'
-    test_save_path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt1_even4_long_test'
+    pathToDataDir = "data/"  # configure if required
+    data_path = [f for f in listdir(pathToDataDir) if isfile(join(pathToDataDir, f))]
+    save_path = join(pathToDataDir, "data_flows.pkl")
 
-    filter_path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt_filtered.pkl'
-    filter_save_path = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt1_even4_filtered'
+    # Load from pcap files - may only need to be run if not already loaded
+    parse_pcap_to_list_n(data_path, save_path)
 
-    filter_path_100 = 'C:\\Users\\nicol\\PycharmProjects\\BA_LTSF_w_Transformer\\data\\UNI1_n\\univ1_pt_filtered_100.pkl'
+    # Optional
+    test_save_path = join(pathToDataDir, "data_flows_test.pkl")
+    create_test_from_full(
+        save_path, test_save_path, amount=50
+    )  # configure if required - look at shuffle if you need it
 
-    aggregation_time = [100]  # 1000 = Milliseconds, 100 = 10xMilliseconds, 10 = 100xMilliseconds, 1 = Seconds
+    final_data_path = save_path if all else test_save_path
+    final_save_path = join(pathToDataDir, "processed_data.pkl")
+    aggregation_time = [1000, 100]
 
-    # _create_split_flow_files()
-    # create_test_from_full(path, test_path, 'TCP', 1000, True)
+    _save_even_gpu(final_data_path, final_save_path, aggr_time=aggregation_time)
 
-    _save_even_gpu(filter_path_100, filter_save_path, aggr_time=aggregation_time)
-
-    print("<<<<<<<<<<<<<<<< Done >>>>>>>>>>>>>>>>")
+    print(f"Saved the data in {final_save_path}!")
+    print(">>>>>>>>>>>>>>>>>>>> Finshed <<<<<<<<<<<<<<<<")
